c87c47f15dfeb014e969e3b0577287136b1997ce
Darius Bžozovskis <darius.bzozo@gmail.com>
2020-09-13 22:42:57 +0200

    Books form field + Image upload functionality with Multer

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const path = require('path');
+const fs = require('fs');
+const multer = require('multer')
+
 const router = express.Router()
 const Book = require('../models/book')
+
+const uploadPath = path.join('public', Book.coverImageBasePath)
+const imageTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg']
+
+const upload = multer({
+    dest: uploadPath,
+    fileFilter: (request, file, callback) => {
+        callback(null, imageTypes.includes(file.mimetype))
+    }
+});
 
 // All Books Route
 router.get('/', async (request, response) => {
@@ -9,16 +23,54 @@ router.get('/', async (request, response) => {
 
 // New Book Route
 router.get('/new', (request, response) => {
-    const book = new Book() 
-    response.render('books/new', {
-        book: book
-    })
+    renderNewPage(response, new Book())
 })
 
 // Create Book Route
-router.post('/', async (request, response) => {
-    response.send('create book route')
+router.post('/', upload.single('cover'), async (request, response) => {
+
+    console.log(request.file)
+
+    const fileName = request.file != null ? request.file.filename : null
+
+    const book = new Book({
+        title: request.body.title,
+        author: request.body.author,
+        publishDate: new Date(request.body.publishDate),
+        pageCount: request.body.pageCount,
+        description: request.body.description,
+        coverImageName: fileName
+    })
+
+    console.log(book);
+
+    try{
+        const newBook = await book.save();
+        response.redirect('books') 
+    } catch{
+ 
+        if(book.coverImageName != null) removeBookCover(book.coverImageName)
+        renderNewPage(response, book, true)
+    }
 })
+
+function renderNewPage(response, book, hasError = false) {
+    
+    const params = {
+        book: book
+    }
+
+    if(hasError) params.errorMessage = 'Error creating book'
+
+    response.render('books/new', params)
+    
+}
+
+function removeBookCover(fileName){
+    fs.unlink(path.join(uploadPath, fileName), err => {
+        if (err) console.error(err)
+    })
+}
 
 
 module.exports = router
